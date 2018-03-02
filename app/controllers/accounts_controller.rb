803c60eb0e41d9d78232fb53747cd4fdc171c3e2
Lindsay Block <lindsay.block05@gmail.com>
2018-03-02 12:22:24 -0700

    finishing up

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,5 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :update, :destroy]
 
   def index
     @accounts = current_user.accounts
@@ -14,25 +15,40 @@ class AccountsController < ApplicationController
   def create
     @account = current_user.accounts.new(account_params)
     if @account.save
+      flash[:success] = 'Account created!'
       redirect_to accounts_path
     else
+      flash[:error] = "Error: #{@account.errors.full_messages.join("/n")}"
       render :new
-    end 
+    end
   end
 
   def edit
   end
 
   def update
+    if @account.update(account_params)
+      redirect_to accounts_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @account.destroy
+    redirect_to accounts_path 
   end
 
   private
 
   def account_params
     params.require(:account).permit(:name, :balance)
+  end
+
+  def set_account
+    @account = current_user.accounts.find(params[:id])
+  end
+
   end
 
 end
